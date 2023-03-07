416e7ffb76680acaf2a95d36dbe7129f1801d614
Edwin574 <owinoedwin574@gmail.com>
2023-03-07 15:36:34 +0300

    added simple jwt

--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -1,8 +1,11 @@
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def getRoutes(request):
     routes=[
         '/api/token',
-        '/api/token/refresh'
+        '/api/token/refresh',
     ]
-    return JsonResponse(routes,safe=false)
+    return Response(routes)
